bd3b05135a9271d8a774ee85537f5bea4865e300
Anjali <anjalirallabandi@gmail.com>
2023-08-11 17:07:08 -0500

    Full GA6

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,20 @@
-const Buttons = () => {
+import styling from "./styling.css";
+import satData from "./satData";
+import App from "../App";
+const Buttons = (props) => {
+  const filterByType = props.filterByType;
+  const setSat = props.setSat;
+  const displaySats = props.displaySats;
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => {
+        return (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+          </button>
+          );
+          })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
 };
